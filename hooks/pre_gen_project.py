5078b4017ed932621ccc24c9da04addee1c0febe
Moritz Mistol <moritz.mistol@gmail.com>
2023-12-29 12:44:47 +0100

    use sys.exit

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,7 +1,9 @@
 import re
+import sys
 
 MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
 package_name = "{{ cookiecutter.package_name }}"
 
 if not re.match(MODULE_REGEX, package_name):
-    raise ValueError(f"ERROR: {package_name} is not a valid Python module name!")
+    print(f"ERROR: {package_name} is not a valid Python module name!")
+    sys.exit(1)
